1b36a48b20630d3c3370cf8fca0d885abd17ce15
patrick lambret <patrick@happy-dev.fr>
2018-12-05 16:48:18 +0100

    added chessboard with drag and drop

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,6 @@
 import "bootstrap";
 import { Chessground } from "chessground";
 
-Chessground(document.getElementById("content"));
+Chessground(document.getElementById("chessgame"));
 
 // Chessground(document.body, config: Config);
