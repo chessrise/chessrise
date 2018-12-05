9b88dde98e981ba7797e4b6bdd0a99729c8b40e5
Jean Charles Fabre <jcharlesfabre@gmail.com>
2018-12-05 18:04:38 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :players
 
   resources :plies do
-    resources :comments, only: [:edit, :show, :new, :destroy]
+    resources :comments, only: [:edit, :show, :new, :destroy, :create]
   end
 
   resources :collections do
