717d1e47ef62ee6ed898aff2fb170c1b0e7165bb
Jean Charles Fabre <jcharlesfabre@gmail.com>
2018-12-12 17:56:27 +0100

    fixed find(1) issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,10 +23,10 @@ p "Creating records"
 user1 = User.create!(email: "jcharlesfabre@gmail.com", password: "password")
 
 
-ImportGamesFromPGNService.new("db/fixtures/wcc18_games.pgn",user1 ).call
+ImportGamesFromPGNService.new("db/fixtures/wcc18_games.pgn",user1, "Carlsen - Caruana" ).call
 ImportGamesFromPGNService.new("db/fixtures/kasparov_games.pgn",user1, "Parties Kasparov" ).call
-ImportGamesFromPGNService.new("db/fixtures/miniatures_games.pgn",user1, "Miniatures" ).call
-ImportGamesFromPGNService.new("db/fixtures/sicilian_khan_games.pgn","Sicilienne Noirs", user1 ).call
+ImportGamesFromPGNService.new("db/fixtures/miniatures_games.pgn",user1).call
+ImportGamesFromPGNService.new("db/fixtures/sicilian_khan_games.pgn",user1).call
 # ImportGamesFromPGNService.new("db/fixtures/my_games.pgn",user1, "Mes parties").call
 
 
