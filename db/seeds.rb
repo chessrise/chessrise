86d3a9b326e04ec285e3aa2e598730ea0555e12f
Jean Charles Fabre <jcharlesfabre@gmail.com>
2018-12-14 10:57:34 +0100

    added collection

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,11 +23,13 @@ p "Creating records"
 user1 = User.create!(email: "jcharlesfabre@gmail.com", password: "password")
 
 
+Collection.create!(name: "Bottes secrètes", user: user1)
+
 ImportGamesFromPGNService.new("db/fixtures/wcc18_games.pgn",user1).call
-ImportGamesFromPGNService.new("db/fixtures/kasparov_games.pgn",user1, "Parties Kasparov" ).call
-ImportGamesFromPGNService.new("db/fixtures/miniatures_games.pgn",user1, "Prix de beauté").call
-ImportGamesFromPGNService.new("db/fixtures/russian_championship_18.pgn",user1, "Russian").call
-ImportGamesFromPGNService.new("db/fixtures/stlouis_rapid.pgn",user1, "St Louis Rapid").call
+ImportGamesFromPGNService.new("db/fixtures/kasparov_games.pgn",user1, "Garry Kasparov" ).call
+ImportGamesFromPGNService.new("db/fixtures/miniatures_games.pgn",user1, "Petites pépites").call
+ImportGamesFromPGNService.new("db/fixtures/russian_championship_18.pgn",user1, "Russian Masters").call
+ImportGamesFromPGNService.new("db/fixtures/stlouis_rapid.pgn",user1, "Rapid games").call
 ImportGamesFromPGNService.new("db/fixtures/sicilian_khan_games.pgn",user1).call
 ImportGamesFromPGNService.new("db/fixtures/my_games.pgn",user1, "My internet games").call
 
